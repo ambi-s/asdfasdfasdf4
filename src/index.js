dece748f23e585fcf509a5faa511bf0979d7c6a5
ambi-s <144171087+ambi-s@users.noreply.github.com>
2023-09-25 22:25:11 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { createBareServer } from "npm:@tomphttp/bare-server-node";
 import express from "npm:express";
 import { createServer } from "node:http";
-import { publicPath } from "https://raw.githubusercontent.com/titaniumnetwork-dev/Ultraviolet-Static/main/lib/index.js";
+import { publicPath } from "https://raw.githubusercontent.com/ambi-s/uv-static/main/lib/index.js";
 import { uvPath } from "npm:@titaniumnetwork-dev/ultraviolet";
 import { join } from "node:path";
 import { hostname } from "node:os";
